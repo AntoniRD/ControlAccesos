4f358e3f3afa86a5d4369c93bd7835858c6c9912
xaxoxavi <xavi.torrens@gmail.com>
2017-02-23 11:49:57 +0100

    Adding Spring Data

--- src/main/java/com/esliceu/dwes/boot/Application.java
+++ src/main/java/com/esliceu/dwes/boot/Application.java
@@ -1,7 +1,13 @@
 package com.esliceu.dwes.boot;
 
+import com.esliceu.dwes.boot.dao.UserRepository;
+import com.esliceu.dwes.boot.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by xavi on 23/02/17.
@@ -9,8 +15,45 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(UserRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+            repository.save(new User("Jack", "Bauer"));
+            repository.save(new User("Chloe", "O'Brian"));
+            repository.save(new User("Kim", "Bauer"));
+            repository.save(new User("David", "Palmer"));
+            repository.save(new User("Michelle", "Dessler"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (User customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            User customer = repository.findOne(1L);
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            for (User bauer : repository.findBySurname("Bauer")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+        };
     }
 
 }
--- src/main/java/com/esliceu/dwes/boot/model/User.java
+++ src/main/java/com/esliceu/dwes/boot/model/User.java
@@ -1,12 +1,27 @@
 package com.esliceu.dwes.boot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 /**
  * Created by xavi on 23/02/17.
  */
+@Entity
 public class User {
 
+    public User(){}
+
+    public User(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String surname;
     private String username;
@@ -61,5 +76,17 @@ public class User {
 
     public void setMailing(Boolean mailing) {
         this.mailing = mailing;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String toString(){
+        return this.id  + ":" + this.surname + ", " + this.name;
     }
 }
