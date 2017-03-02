c153bbc5cae2625a56ec927916f1542c6adc314d
xaxoxavi <xavi.torrens@gmail.com>
2017-03-02 13:46:40 +0100

    Deleting old Pojos and new bbdd configuration

--- src/main/java/com/esliceu/dwes/boot/Application.java
+++ src/main/java/com/esliceu/dwes/boot/Application.java
@@ -18,40 +18,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public CommandLineRunner demo(UserRepository repository) {
-        return (args) -> {
-            // save a couple of customers
-            repository.save(new User("Jack", "Bauer"));
-            repository.save(new User("Chloe", "O'Brian"));
-            repository.save(new User("Kim", "Bauer"));
-            repository.save(new User("David", "Palmer"));
-            repository.save(new User("Michelle", "Dessler"));
-
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            for (User customer : repository.findAll()) {
-                log.info(customer.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            User customer = repository.findOne(1L);
-            log.info("Customer found with findOne(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):");
-            log.info("--------------------------------------------");
-            for (User bauer : repository.findBySurname("Bauer")) {
-                log.info(bauer.toString());
-            }
-            log.info("");
-        };
-    }
-
 }
