a3e7e37d21385b9d523c23e5f8a4e231dafcd228
xaxoxavi <xavi.torrens@gmail.com>
2017-03-03 17:05:55 +0100

    Hibernate auto update none and making a relationship with Rol

--- src/main/java/com/esliceu/dwes/boot/dao/UsuarioRepository.java
+++ src/main/java/com/esliceu/dwes/boot/dao/UsuarioRepository.java
@@ -1,12 +1,20 @@
 package com.esliceu.dwes.boot.dao;
 
 import com.esliceu.dwes.boot.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by xavi on 2/03/17.
  */
 public interface UsuarioRepository extends CrudRepository<Usuario,String> {
 
+    List<Usuario> findByNombre(String nombre);
 
+    List<Usuario> findByNombreAndContraseña(String nombre, String contraseña);
+
+    @Query(value = "SELECT * FROM auth.user_roles,auth.users where users.user_name=user_roles.user_name and user_pass = ?1", nativeQuery = true)
+    List<Usuario> findUser(String password);
 }
